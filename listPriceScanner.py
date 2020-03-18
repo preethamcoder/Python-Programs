65f40c8a7d9e618738e59829b42400296c8ee9a2
preethamjava123 <46249739+preethamjava123@users.noreply.github.com>
2020-03-18 15:43:46 -0400

    Add files via upload

---
+++ listPriceScanner.py
@@ -0,0 +1,13 @@
+
+
+x = [1.12, 1.34, 1.58, 2.61, 2.34, 5.64, 8.84]
+
+a = ["Babba", "Apple", "Mango", "Orange", "Okra", "Muskmellon", "Green Babba"]
+
+print a
+
+print x
+
+print("These are prices of products at respective idices")
+
+
